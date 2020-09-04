eec8288841e5a97a5911731753d9d0b4c0e87ecf
Krunal.Dave <krunal.dave@infostretch.com>
2020-09-04 11:23:55 +0530

    partial unit test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,49 @@
 /**
  * @format
  */
-
-import 'react-native';
 import React from 'react';
-import App from '../App';
+import Enzyme,{shallow,mount} from 'enzyme';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import ImageList from '../src/components/ImageList';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+import Adapter from 'enzyme-adapter-react-16';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import App from '../App';
+ 
+const middlewares = []
+const mockStore = configureStore(middlewares)
+Enzyme.configure({ adapter: new Adapter() });
+const initialProps = {
+    imageCount:10,
+    images:[],
+    length:0,
+}
+
+const store = mockStore({
+  images: [],
+  first: 0,
+  second: 0,  
+});
+
+describe('renders correctly', () => {
+  const setUpFn = props => {
+    return mount(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  };
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setUpFn();
+  });
+const container = shallow(<ImageList {...initialProps} />);
+
+const flatList = wrapper.find('FlatList');
+
+const item = flatList.renderProp('renderItem')({ item: yourData });
+expect(item).toMatchSnapshot();
 });
